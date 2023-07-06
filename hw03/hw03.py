d2aa3de5c5d2a81df69944e8ad72a7d6eca2c35c
zizheng02 <zizheng.wang02@gmail.com>
2023-07-06 12:54:18 +0800

    finish🎉: hw03

---
+++ hw03/hw03.py
@@ -0,0 +1,153 @@
+HW_SOURCE_FILE = __file__
+
+
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    if n==0:
+        return 0
+    return 1+num_eights(n//10) if n%10==8 else num_eights(n//10)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    # 控制方向
+    def flag(n):
+        if n==1:
+            return 1
+        if num_eights(n) or n%8==0:
+            return -flag(n-1)
+        return flag(n-1)
+    if n==1:
+        return 1
+    return pingpong(n-1) + flag(n-1)
+
+# 非递归版本
+# def ping_pong(n):
+#     i=1
+#     flag=1
+#     num=0
+#     for i in range(1,n+1):
+#         num+=flag
+#         if num_eights(i) or i%8==0:
+#             flag*=-1
+        
+#     return num
+
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+
+def count_coins(change):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    def helper(change, max_coin):
+        if change<0:
+            return 0
+        elif change==0:
+            return 1
+        elif max_coin==None:
+            return 0
+        else: 
+            return helper(change-max_coin, max_coin) + helper(change, next_larger_coin(max_coin))
+    return helper(change, 1)
+        
+    
